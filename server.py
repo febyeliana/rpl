3453bda78eb7f8a7b15de564f67028b299d7c221
osboxes.org <osboxes@manager.vanilla.com>
2019-11-09 03:28:04 +0700

    Third commit server module. Added post and update routes

--- server.py
+++ server.py
@@ -1,18 +1,62 @@
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, jsonify
 from detail_beasiswa import DetailBeasiswa
+from mahasiswa import Mahasiswa
+from penyedia_beasiswa import PenyediaBeasiswa
+from pilihan_beasiswa import PilihanBeasiswa
 
 app = Flask(__name__)
 
+#mahasiswa routes
+#lihat semua mahasiswa
+@app.route('/mahasiswa', methods = ['GET','POST'])
+def mahasiswa():
+	#method GET
+	if request.method == 'GET':
+		resp = make_response(Mahasiswa.lihatSemua(),200)
+	#method POST
+	elif request.method == 'POST':
+		resp = make_response(Mahasiswa.insertDetail(request.data),200)
+	resp.mimetype = "application/json"
+	return resp
+
+#filter by NIM
+@app.route('/mahasiswa/nim/<nim>', methods = ['GET'])
+def mahasiswa_nim(nim):
+	resp = make_response(Mahasiswa.filterNIM(nim),200)
+	resp.mimetype = "application/json"
+	return resp
+
+#penyedia beasiswa routes
+#lihat semua penyedia beasiswa
+@app.route('/penyediabeasiswa', methods = ['GET','POST'])
+def penyediabeasiswa():
+	#method GET
+	if request.method == 'GET':
+		resp = make_response(PenyediaBeasiswa.lihatSemua(),200)
+	#method POST
+	elif request.method == 'POST':
+		resp = make_response(PenyediaBeasiswa.insertDetail(request.data),200)
+	resp.mimetype = "application/json"
+	return resp
+
+#filter by ID
+@app.route('/penyediabeasiswa/id/<id_penyedia>', methods = ['GET'])
+def penyediabeasiswa_id(id_penyedia):
+	resp = make_response(PenyediaBeasiswa.filterID(id_penyedia),200)
+	resp.mimetype = "application/json"
+	return resp
+
+#detail beasiswa routes
 # lihat semua detail beasiswa
-@app.route('/detailbeasiswa',methods=['GET'])
+@app.route('/detailbeasiswa',methods=['GET','POST'])
 def detailbeasiswa():
 	#method GET
 	if request.method == 'GET':
 		resp = make_response(DetailBeasiswa.lihatSemua(),200)
-	resp.mimetype = "application/json" 
 	#method POST
-
-
+	elif request.method == 'POST':
+		resp = make_response(DetailBeasiswa.insertDetail(request.data),200)
+	resp.mimetype = "application/json"
 	return resp
 
 #filter by fakultas
@@ -117,6 +161,36 @@ def detailbeasiswa_semester_gpa(semester,gpa):
 @app.route('/detailbeasiswa/gpa/<gpa>', methods = ['GET'])
 def detailbeasiswa_gpa(gpa):
 	resp = make_response(DetailBeasiswa.filterMinGPA(gpa),200)
+	resp.mimetype = "application/json"
+	return resp
+
+#pilihan beasiswa routes
+@app.route('/pilihanbeasiswa', methods = ['GET','POST'])
+def pilihanbeasiswa():
+	#method GET
+	if request.method == 'GET':
+		resp = make_response(PilihanBeasiswa.lihatSemua(),200)
+	#method POST
+	elif request.method == 'POST':
+		resp = make_response(PilihanBeasiswa.insertDetail(request.data),200)
+	resp.mimetype = "application/json"
+	return resp
+
+@app.route('/pilihanbeasiswa/id/<id_penyedia>', methods = ['GET'])
+def pilihanbeasiswa_id(id_penyedia):
+	resp = make_response(PilihanBeasiswa.filterID(id_penyedia),200)
+	resp.mimetype = "application/json"
+	return resp
+
+@app.route('/pilihanbeasiswa/nim/<nim>', methods = ['GET'])
+def pilihanbeasiswa_nim(nim):
+	resp = make_response(PilihanBeasiswa.filterNIM(nim),200)
+	resp.mimetype = "application/json"
+	return resp
+
+@app.route('/pilihanbeasiswa/id/<id_penyedia>/nim/<nim>',methods=['PUT'])
+def pilihanbeasiswa_nim_id(id_penyedia,nim):
+	resp = make_response(PilihanBeasiswa.updateStatusSeleksi(request.data,id_penyedia,nim),200)
 	resp.mimetype = "application/json"
 	return resp
 
