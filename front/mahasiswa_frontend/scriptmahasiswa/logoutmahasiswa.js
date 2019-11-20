3dc89e029414a32d0d1671edd23f4a9376258b70
William <fraptorwh98@WH.localdomain>
2019-11-20 20:58:43 +0700

    Rearrange files

--- front/mahasiswa_frontend/scriptmahasiswa/logoutmahasiswa.js
+++
@@ -1,5 +0,0 @@
-const logout = () => {
-  window.localStorage.clear();
-  let urlPart = window.location.href.split('/');
-  window.location = urlPart.splice(0, urlPart.length-1).join('/') + '/login.html';
-};
