f897d2a7a9c154998a007f21263bfabfe0646f5c
William <fraptorwh98@WH.localdomain>
2019-11-20 20:56:53 +0700

    kumpulan file untuk bagian front end mahasiswa

---
+++ front/mahasiswa_frontend/scriptmahasiswa/login.js
@@ -0,0 +1,19 @@
+const login = async () => {
+  let username = document.getElementById('username').value;
+  let password = document.getElementById('password').value;
+
+  let response = await fetch('http://3.227.193.57:9000/mahasiswa/login');
+  let data = await response.json();
+
+  for (let item of data) {
+    if (item.username === username && item.password === password) {
+      let urlPart = window.location.href.split('/');
+      window.location = urlPart.splice(0, urlPart.length-1).join('/') + '/infomahasiswa.html';
+      window.localStorage.setItem('username', username);
+      window.localStorage.setItem('password', password);
+      return;
+    }
+  }
+
+  alert("Invalid login!")
+};
